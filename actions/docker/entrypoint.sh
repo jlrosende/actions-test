70a98fdd91a225e345d1aa4d2506bfd728ea2d1f
jlrosende <10084165+jlrosende@users.noreply.github.com>
2025-08-30 13:27:11 +0200

    seach folderas and content

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -4,3 +4,18 @@ echo "hola $1"
 
 env
 
+echo "/github"
+ls -la /github
+echo "--------------------------------"
+
+echo "/github/workspace"
+ls -la /github/workspace
+echo "--------------------------------"
+
+echo "/github/home"
+ls -la /github/home
+echo "--------------------------------"
+
+echo "/github/file_commands"
+ls -la /github/file_commands
+echo "--------------------------------"
