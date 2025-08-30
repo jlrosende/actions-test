84001ce06f206db310a12695ac2e93d7a6db23e0
jlrosende <10084165+jlrosende@users.noreply.github.com>
2025-08-30 13:48:15 +0200

    add basic memory

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -2,26 +2,26 @@
 
 getent passwd $USER
 
-echo "/github"
-ls -la /github
-echo "--------------------------------"
-
-echo "/github/workspace"
-ls -la /github/workspace
-echo "--------------------------------"
-
 echo "/github/home"
 ls -la /github/home
 echo "--------------------------------"
 
-echo "/github/file_commands"
-ls -la /github/file_commands
+uv tool install basic-memory
+
+echo "# My Note\n\nThis is a test note" | basic-memory tool write-note \
+  --title "Test Note" --folder "notes"
 echo "--------------------------------"
 
 mkdir -p /github/home/cache
 
 ls -la /github/home/cache
+echo "--------------------------------"
 
 touch "/github/home/cache/$(date +"%Y-%m-%d-%H-%M-%S")"
 
 ls -la /github/home/cache
+echo "--------------------------------"
+
+echo "/github/home"
+ls -la /github/home
+echo "--------------------------------"
